d7232fe14e3aa862c8a0fa053aa696a403b1c990
Farhan Ali <officialofun@gmail.com>
2026-06-17 18:10:04 +0500

    feat: restructure assets and add HTML template for code execution
    - Move eruda.min.js from assets/js/ to assets/data/
    - Add template.html with eruda integration and __USER_SCRIPT__ placeholder
    - Update Constants: rename FILE_EXTRACTED → DATA_EXTRACTED
    - Add WEBVIEW_TEMPLATE, CODE_JS constants; remove TMP_JS

--- app/src/main/java/com/github/farhaanaliii/jsrun/Handler/Constants.java
+++ app/src/main/java/com/github/farhaanaliii/jsrun/Handler/Constants.java
@@ -2,8 +2,9 @@ package com.github.farhaanaliii.jsrun.Handler;
 
 public class Constants {
     public static final String SHARED_PREFS = "sharedPrefs";
-    public static final String FILE_EXTRACTED = "fileExtracted";
-    public static final String WEBVIEW_MAIN_FILE = "index.html";
-    public static final String ERUDA_JS = "eruda.min.js";
-    public static final String TMP_JS = "tmp_js_file.txt";
+    public static final String DATA_EXTRACTED = "dataExtracted";
+    public static final String WEBVIEW_MAIN_FILE = "data/index.html";
+    public static final String WEBVIEW_TEMPLATE = "data/template.html";
+    public static final String ERUDA_JS = "data/eruda.min.js";
+    public static final String CODE_JS = "code.js";
 }
